e6a54a3703b49a7bf05fca132d2211cae2ab73af
Cody Garvin <emig647@gmail.com>
2020-12-08 15:48:25 -0800

    Cleaned up lifecycles

--- Sources/Segment/Extensions/iOSLifecycleEvents.swift
+++ Sources/Segment/Extensions/iOSLifecycleEvents.swift
@@ -9,22 +9,65 @@ import Foundation
 #if os(iOS) || os(watchOS) || os(tvOS)
 import UIKit
 
-class iOSLifeCycleEvents: Extension {
+//protocol iOSLifeCycleCoordinator: ExtensionCoordinator {
+//    func didEnterBackgroundNotification()
+//}
+
+//extension iOSLifeCycleCoordinator {
+//    func didEnterBackgroundNotification() { }
+//}
+
+class iOSLifeCycleEvents: Extension  {
+    
     var type: ExtensionType
     var analytics: Analytics? = nil
     
     private var application: UIApplication
+    private var appNotifications: [NSNotification.Name] = [UIApplication.didEnterBackgroundNotification,
+                                                           UIApplication.willEnterForegroundNotification,
+                                                           UIApplication.didFinishLaunchingNotification,
+                                                           UIApplication.didBecomeActiveNotification,
+                                                           UIApplication.willResignActiveNotification,
+                                                           UIApplication.didReceiveMemoryWarningNotification,
+                                                           UIApplication.willTerminateNotification,
+                                                           UIApplication.significantTimeChangeNotification,
+                                                           UIApplication.backgroundRefreshStatusDidChangeNotification]
+    
+    
 
     required init(type: ExtensionType) {
-        self.type = .before
+        self.type = type // needs to be before
         application = UIApplication.shared
         
         setupListeners()
+
+    }
+    
+    @objc
+    func notificationResponse(notification: NSNotification) {
+        print("Notification Happened: \(notification)")
+        
+        
+        
+    }
+    
+    internal func addResponder() {
+        
+    }
+    
+    internal func removeResponder() {
+        
     }
     
     func setupListeners() {
         // do all your subscription to lifecycle shit here
         // .. listener ends up calling appicationDidFinishLaunching
+        // Configure the current life cycle events
+        let notificationCenter = NotificationCenter.default
+        for notification in appNotifications {
+            notificationCenter.addObserver(self, selector: #selector(notificationResponse(notification:)), name: notification, object: application)
+        }
+
     }
     
     func applicationDidFinishLaunching(notification: Notification) {
