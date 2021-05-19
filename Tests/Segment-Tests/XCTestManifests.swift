cecde32fb364f1c14f2adddc1cdc8610d35eca4e
Brandon Sneed <brandon.sneed@segment.com>
2021-05-19 13:21:59 -0700

    more CI fixes for linux

--- Tests/Segment-Tests/XCTestManifests.swift
+++ Tests/Segment-Tests/XCTestManifests.swift
@@ -1,5 +1,9 @@
 import XCTest
 
+// this isn't needed anymore on linux, however
+// i'm leaving it here because of the unusual deprecation
+// messages `swift test` is producing.
+
 /*#if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
