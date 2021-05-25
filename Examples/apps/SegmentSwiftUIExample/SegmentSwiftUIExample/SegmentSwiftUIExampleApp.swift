6d684c795ed75ad2940fcf82234dcd50dec5452c
Cody Garvin <emig647@gmail.com>
2021-05-25 15:21:48 -0700

    Added SwiftUI example

---
+++ Examples/apps/SegmentSwiftUIExample/SegmentSwiftUIExample/SegmentSwiftUIExampleApp.swift
@@ -0,0 +1,25 @@
+//
+//  SegmentSwiftUIExampleApp.swift
+//  SegmentSwiftUIExample
+//
+//  Created by Cody Garvin on 5/24/21.
+//
+
+import SwiftUI
+import Segment
+
+@main
+struct SegmentSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+extension Analytics {
+    static var main = Analytics(configuration:
+                                    Configuration(writeKey: "ABCD")
+                                    .flushAt(3)
+                                    .trackApplicationLifecycleEvents(true))
+}
