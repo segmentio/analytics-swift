b88edfe8830cd344bedac89070a9b25eeb924de3
Brandon Sneed <brandon.sneed@segment.com>
2021-07-01 12:25:38 -0700

    added watchOS lifecycle stuff

---
+++ Sources/Segment/Plugins/Platforms/watchOS/watchOSDelegation.swift
@@ -0,0 +1,56 @@
+//
+//  File.swift
+//  
+//
+//  Created by Brandon Sneed on 6/24/21.
+//
+
+#if os(watchOS)
+
+import Foundation
+import WatchKit
+
+// MARK: - Remote Notifications
+
+public protocol RemoteNotifications: Plugin {
+    func registeredForRemoteNotifications(deviceToken: Data)
+    func failedToRegisterForRemoteNotification(error: Error?)
+    func receivedRemoteNotification(userInfo: [String: Any])
+}
+
+extension RemoteNotifications {
+    public func registeredForRemoteNotifications(deviceToken: Data) {}
+    public func failedToRegisterForRemoteNotification(error: Error?) {}
+    public func receivedRemoteNotification(userInfo: [String: Any]) {}
+}
+
+extension Analytics {
+    public func registeredForRemoteNotifications(deviceToken: Data) {
+        setDeviceToken(deviceToken.hexString)
+        
+        apply { plugin in
+            if let p = plugin as? RemoteNotifications {
+                p.registeredForRemoteNotifications(deviceToken: deviceToken)
+            }
+        }
+    }
+    
+    public func failedToRegisterForRemoteNotification(error: Error?) {
+        apply { plugin in
+            if let p = plugin as? RemoteNotifications {
+                p.failedToRegisterForRemoteNotification(error: error)
+            }
+        }
+    }
+    
+    public func receivedRemoteNotification(userInfo: [String: Any]) {
+        apply { plugin in
+            if let p = plugin as? RemoteNotifications {
+                p.receivedRemoteNotification(userInfo: userInfo)
+            }
+        }
+    }
+
+}
+
+#endif
--- Sources/Segment/Plugins/Platforms/watchOS/watchOSLifecycleEvents.swift
+++ Sources/Segment/Plugins/Platforms/watchOS/watchOSLifecycleEvents.swift
@@ -5,25 +5,75 @@
 //  Created by Brandon Sneed on 3/29/21.
 //
 
-import Foundation
-
 #if os(watchOS)
 
-// Work in progress ... TBD
+import Foundation
+import WatchKit
 
-class watchOSLifecycleMonitor: PlatformPlugin {
-    static var specificName: String = "Segment_watchOSLifecycleMonitor"
+class watchOSLifecycleEvents: PlatformPlugin, watchOSLifecycle {
+    static var versionKey = "SEGVersionKey"
+    static var buildKey = "SEGBuildKeyV2"
     
-    var type: PluginType
-    var name: String
+    static var specificName: String = "Segment_watchOSLifecycleEvents"
+    
+    let type: PluginType
+    let name: String
     var analytics: Analytics?
     
-    required init(name: String) {
-        self.type = .utility
+    public required init(name: String) {
         self.name = name
+        self.type = .before
     }
     
-    // ...
+    func applicationDidFinishLaunching(watchExtension: WKExtension) {
+        if analytics?.configuration.values.trackApplicationLifecycleEvents == false {
+            return
+        }
+        
+        let previousVersion = UserDefaults.standard.string(forKey: Self.versionKey)
+        let previousBuild = UserDefaults.standard.string(forKey: Self.buildKey)
+        
+        let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        let currentBuild = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
+        
+        if previousBuild == nil {
+            analytics?.track(name: "Application Installed", properties: [
+                "version": currentVersion ?? "",
+                "build": currentBuild ?? ""
+            ])
+        } else if currentBuild != previousBuild {
+            analytics?.track(name: "Application Updated", properties: [
+                "previous_version": previousVersion ?? "",
+                "previous_build": previousBuild ?? "",
+                "version": currentVersion ?? "",
+                "build": currentBuild ?? ""
+            ])
+        }
+        
+        analytics?.track(name: "Application Opened", properties: [
+            "from_background": false,
+            "version": currentVersion ?? "",
+            "build": currentBuild ?? ""
+        ])
+        
+        UserDefaults.standard.setValue(currentVersion, forKey: Self.versionKey)
+        UserDefaults.standard.setValue(currentBuild, forKey: Self.buildKey)
+    }
+    
+    func applicationWillEnterForeground(watchExtension: WKExtension) {
+        if analytics?.configuration.values.trackApplicationLifecycleEvents == false {
+            return
+        }
+        
+        let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        let currentBuild = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
+        
+        analytics?.track(name: "Application Opened", properties: [
+            "from_background": true,
+            "version": currentVersion ?? "",
+            "build": currentBuild ?? ""
+        ])
+    }
 }
 
 #endif
