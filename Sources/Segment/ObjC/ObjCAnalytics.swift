0c883af64399916a95c19cd771a90b6977ba23e6
Brandon Sneed <brandon.sneed@segment.com>
2021-08-13 13:12:51 -0700

    Added convenience methods

--- Sources/Segment/ObjC/ObjCAnalytics.swift
+++ Sources/Segment/ObjC/ObjCAnalytics.swift
@@ -25,11 +25,27 @@ public class ObjCAnalytics: NSObject {
 
 @objc
 extension ObjCAnalytics {
-    @objc(track:properties:)
-    public func track(name: String, properties: [String: Any]? = nil) {
-        analytics.track(name: name, properties: properties)
+    @objc(track:)
+    public func track(name: String) {
+        track(name: name, properties: nil)
     }
     
+
+    @objc(track:properties:)
+    public func track(name: String, properties: [String: Any]?) {
+        analytics.track(name: name, properties: properties)
+    }
+
+    /// Associate a user with their unique ID and record traits about them.
+    /// - Parameters:
+    ///   - userId: A database ID (or email address) for this user. If you don't have a userId
+    ///     but want to record traits, you should pass nil. For more information on how we
+    ///     generate the UUID and Apple's policies on IDs, see https://segment.io/libraries/ios#ids
+    @objc(identify:)
+    public func identify(userId: String) {
+        identify(userId: userId, traits: nil)
+    }
+
     /// Associate a user with their unique ID and record traits about them.
     /// - Parameters:
     ///   - userId: A database ID (or email address) for this user. If you don't have a userId
@@ -37,11 +53,11 @@ extension ObjCAnalytics {
     ///     generate the UUID and Apple's policies on IDs, see https://segment.io/libraries/ios#ids
     ///   - traits: A dictionary of traits you know about the user. Things like: email, name, plan, etc.
     @objc(identify:traits:)
-    public func identify(userId: String, traits: [String: AnyHashable]? = nil) {
+    public func identify(userId: String, traits: [String: AnyHashable]?) {
         analytics.identify(userId: userId, traits: traits)
     }
     
-    /// Associate a user with traits about them.
+    /// Associate the current user with traits about them.
     /// - Parameters:
     ///   - traits: A dictionary of traits you know about the user. Things like: email, name, plan, etc.
     @objc(identify:)
@@ -52,19 +68,43 @@ extension ObjCAnalytics {
     /// Track a screen change with a title, category and other properties.
     /// - Parameters:
     ///   - screenTitle: The title of the screen being tracked.
+    @objc(screen:)
+    public func screen(screenTitle: String) {
+        screen(screenTitle: screenTitle, category: nil, properties: nil)
+    }
+    
+    /// Track a screen change with a title, category and other properties.
+    /// - Parameters:
+    ///   - screenTitle: The title of the screen being tracked.
+    ///   - category: A category to the type of screen if it applies.
+    @objc(screen:category:)
+    public func screen(screenTitle: String, category: String?) {
+        analytics.screen(screenTitle: screenTitle, category: category, properties: nil)
+    }
+    /// Track a screen change with a title, category and other properties.
+    /// - Parameters:
+    ///   - screenTitle: The title of the screen being tracked.
     ///   - category: A category to the type of screen if it applies.
     ///   - properties: Any extra metadata associated with the screen. e.g. method of access, size, etc.
     @objc(screen:category:properties:)
     public func screen(screenTitle: String, category: String?, properties: [String: AnyHashable]?) {
         analytics.screen(screenTitle: screenTitle, category: category, properties: properties)
     }
-    
+
+    /// Associate a user with a group such as a company, organization, project, etc.
+    /// - Parameters:
+    ///   - groupId: A unique identifier for the group identification in your system.
+    @objc(group:traits:)
+    public func group(groupId: String) {
+        group(groupId: groupId, traits: nil)
+    }
+
     /// Associate a user with a group such as a company, organization, project, etc.
     /// - Parameters:
     ///   - groupId: A unique identifier for the group identification in your system.
     ///   - traits: Traits of the group you may be interested in such as email, phone or name.
     @objc(group:traits:)
-    public func group(groupId: String, traits: [String: AnyHashable]? = nil) {
+    public func group(groupId: String, traits: [String: AnyHashable]?) {
         analytics.group(groupId: groupId, traits: traits)
     }
 }
