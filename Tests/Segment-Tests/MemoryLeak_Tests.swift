93eebeb99b76fb6e453a6acdf037a3059d23fe29
Brandon Sneed <brandon@redf.net>
2022-10-17 12:25:49 -0700

    Added leak testing

--- Tests/Segment-Tests/MemoryLeak_Tests.swift
+++ Tests/Segment-Tests/MemoryLeak_Tests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Segment
 
 final class MemoryLeak_Tests: XCTestCase {
 
@@ -17,19 +18,83 @@ final class MemoryLeak_Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    func testLeaksVerbose() throws {
+        let analytics = Analytics(configuration: Configuration(writeKey: "1234"))
+        
+        waitUntilStarted(analytics: analytics)
+        analytics.track(name: "test")
+        
+        RunLoop.main.run(until: Date(timeIntervalSinceNow: 1))
+        
+        let segmentDest = analytics.find(pluginType: SegmentDestination.self)!
+        let destMetadata = segmentDest.timeline.find(pluginType: DestinationMetadataPlugin.self)!
+        let startupQueue = analytics.find(pluginType: StartupQueue.self)!
+        let segmentLog = analytics.find(pluginType: SegmentLog.self)!
+         
+        let context = analytics.find(pluginType: Context.self)!
+        let deviceToken = analytics.find(pluginType: DeviceToken.self)!
+        
+        #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
+        let iosLifecycle = analytics.find(pluginType: iOSLifecycleEvents.self)!
+        let iosMonitor = analytics.find(pluginType: iOSLifecycleMonitor.self)!
+        #elseif os(watchOS)
+        let watchLifecycle = analytics.find(pluginType: watchOSLifecycleEvents.self)!
+        let watchMonitor = analytics.find(pluginType: watchOSLifecycleMonitor.self)!
+        #elseif os(macOS)
+        let macLifecycle = analytics.find(pluginType: macOSLifecycleEvents.self)!
+        let macMonitor = analytics.find(pluginType: macOSLifecycleMonitor.self)!
+        #endif
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        analytics.remove(plugin: startupQueue)
+        analytics.remove(plugin: segmentLog)
+        analytics.remove(plugin: segmentDest)
+        segmentDest.remove(plugin: destMetadata)
+         
+        analytics.remove(plugin: context)
+        analytics.remove(plugin: deviceToken)
+        #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
+        analytics.remove(plugin: iosLifecycle)
+        analytics.remove(plugin: iosMonitor)
+        #elseif os(watchOS)
+        analytics.remove(plugin: watchLifecycle)
+        analytics.remove(plugin: watchMonitor)
+        #elseif os(macOS)
+        analytics.remove(plugin: macLifecycle)
+        analytics.remove(plugin: macMonitor)
+        #endif
+
+        RunLoop.main.run(until: Date(timeIntervalSinceNow: 1))
+
+        checkIfLeaked(segmentLog)
+        checkIfLeaked(segmentDest)
+        checkIfLeaked(destMetadata)
+        checkIfLeaked(startupQueue)
+        
+        checkIfLeaked(deviceToken)
+        checkIfLeaked(context)
+        #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
+        checkIfLeaked(iosLifecycle)
+        checkIfLeaked(iosMonitor)
+        #elseif os(watchOS)
+        checkIfLeaked(watchLifecycle)
+        checkIfLeaked(watchMonitor)
+        #elseif os(macOS)
+        checkIfLeaked(macLifecycle)
+        checkIfLeaked(macMonitor)
+        #endif
+        
+        checkIfLeaked(analytics)
+    }
+    
+    func testLeaksSimple() throws {
+        let analytics = Analytics(configuration: Configuration(writeKey: "1234"))
+        
+        waitUntilStarted(analytics: analytics)
+        analytics.track(name: "test")
+        
+        RunLoop.main.run(until: Date(timeIntervalSinceNow: 1))
+
+        checkIfLeaked(analytics)
     }
 
 }
