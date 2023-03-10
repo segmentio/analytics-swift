10b5d29dc93546650528c4dc87c00830c22ad983
Brandon Sneed <brandon@redf.net>
2023-03-09 17:49:46 -0800

    Handle set explicitly.

--- Sources/Segment/Utilities/JSON.swift
+++ Sources/Segment/Utilities/JSON.swift
@@ -59,7 +59,9 @@ public enum JSON: Equatable {
             self = .string(string)
         case let bool as Bool:
             self = .bool(bool)
-        case let array as any Collection:
+        case let aSet as Set<AnyHashable>:
+            self = .array(try aSet.map(JSON.init))
+        case let array as Array<Any>:
             self = .array(try array.map(JSON.init))
         case let object as [String: Any]:
             self = .object(try object.mapValues(JSON.init))
