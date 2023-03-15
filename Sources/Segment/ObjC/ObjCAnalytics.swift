2c80798c09e08171452b6df1d8e7e4e2104d90d9
Brandon Sneed <brandon@redf.net>
2023-03-14 17:37:52 -0700

    Add source/dest middleware equivalents for ObjC (#204)
    * Add source/dest middleware equivalents for ObjC
    
    * Added ifdef for linux
    
    * Fixed xcodeproj

--- Sources/Segment/ObjC/ObjCAnalytics.swift
+++ Sources/Segment/ObjC/ObjCAnalytics.swift
@@ -129,6 +129,63 @@ extension ObjCAnalytics {
     }
 }
 
+// MARK: - ObjC Plugin Functionality
+
+@objc
+extension ObjCAnalytics {
+    /// This method allows you to add middleware to an Analytics instance, similar to Analytics-iOS.
+    /// However, it is **strongly encouraged** that Enrichments/Plugins/Middlewares be written in swift
+    /// to avoid the overhead of type conversion back and forth.  This exists solely for compatibility
+    /// purposes.
+    ///
+    /// Example:
+    ///    [self.analytics addSourceMiddleware:^NSDictionary<NSString *,id> * _Nullable(NSDictionary<NSString *,id> * _Nullable event) {
+    ///        // drop all events named booya
+    ///        NSString *eventType = event[@"type"];
+    ///        if ([eventType isEqualToString:@"track"]) {
+    ///            NSString *eventName = event[@"event"];
+    ///            if ([eventName isEqualToString:@"booya"]) {
+    ///                return nil;
+    ///            }
+    ///        }
+    ///        return event;
+    ///    }];
+    ///
+    /// - Parameter middleware: The middleware to execute at the source level.
+    @objc(addSourceMiddleware:)
+    public func addSourceMiddleware(middleware: @escaping ((_ event: [String: Any]?) -> [String: Any]?)) {
+        analytics.add(plugin: ObjCShimPlugin(middleware: middleware))
+    }
+    
+    /// This method allows you to add middleware to an Analytics instance, similar to Analytics-iOS.
+    /// However, it is **strongly encouraged** that Enrichments/Plugins/Middlewares be written in swift
+    /// to avoid the overhead of type conversion back and forth.  This exists solely for compatibility
+    /// purposes.
+    ///
+    /// Example:
+    ///    [self.analytics addDestinationMiddleware:^NSDictionary<NSString *,id> * _Nullable(NSDictionary<NSString *,id> * _Nullable event) {
+    ///        // drop all events named booya on the amplitude destination
+    ///        NSString *eventType = event[@"type"];
+    ///        if ([eventType isEqualToString:@"track"]) {
+    ///            NSString *eventName = event[@"event"];
+    ///            if ([eventName isEqualToString:@"booya"]) {
+    ///                return nil;
+    ///            }
+    ///        }
+    ///        return event;
+    ///    }, forKey: @"Amplitude"];
+    ///
+    /// - Parameters:
+    ///   - middleware: The middleware to execute at the source level.
+    ///   - destinationKey: A string value representing the destination.  ie: @"Amplitude"
+    @objc(addDestinationMiddleware:forKey:)
+    public func addDestinationMiddleware(middleware: @escaping ((_ event: [String: Any]?) -> [String: Any]?), destinationKey: String) {
+        // couldn't find the destination they wanted
+        guard let dest = analytics.find(key: destinationKey) else { return }
+        _ = dest.add(plugin: ObjCShimPlugin(middleware: middleware))
+    }
+}
+
 // MARK: - ObjC Peripheral Functionality
 
 @objc
