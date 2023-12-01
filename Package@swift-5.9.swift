70979035779f290aa4c31d666366b370a87ff25b
Brian Michel <brian.michel@gmail.com>
2023-11-30 20:00:21 -0500

    Update json encoder version

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/segmentio/sovran-swift.git", from: "1.1.0"),
-        .package(url: "https://github.com/segmentio/jsonsafeencoder-swift.git", from: "1.0.0")
+        .package(url: "https://github.com/segmentio/jsonsafeencoder-swift.git", from: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
