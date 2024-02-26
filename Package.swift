4b171a3ef0214195b284d787db5d3df020ef436c
Alan Charles <50601149+alanjcharles@users.noreply.github.com>
2024-02-26 15:11:25 -0700

    update sovran to include thread safety (#298)

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/segmentio/sovran-swift.git", from: "1.1.0"),
+        .package(url: "https://github.com/segmentio/sovran-swift.git", from: "1.1.1"),
         .package(url: "https://github.com/segmentio/jsonsafeencoder-swift.git", from: "1.0.0")
     ],
     targets: [
