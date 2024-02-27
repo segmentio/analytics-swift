e05a5acb6d30f5b3e0dca61df8170cb127e0be8e
Alan Charles <50601149+alanjcharles@users.noreply.github.com>
2024-02-27 15:52:19 -0700

    Update Package@swift-5.9.swift
    remove merge conflict label

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -21,13 +21,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-<<<<<<< HEAD
-        .package(url: "https://github.com/segmentio/sovran-swift.git", from: "1.1.0"),
-        .package(url: "https://github.com/segmentio/jsonsafeencoder-swift.git", from: "1.0.1")
-=======
         .package(url: "https://github.com/segmentio/sovran-swift.git", from: "1.1.1"),
-        .package(url: "https://github.com/segmentio/jsonsafeencoder-swift.git", from: "1.0.0")
->>>>>>> main
+        .package(url: "https://github.com/segmentio/jsonsafeencoder-swift.git", from: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
