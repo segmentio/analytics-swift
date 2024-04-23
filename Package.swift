a1b519eb89f972fbe0d47d8027097ccfa8a9ae74
Brandon Sneed <brandon@redf.net>
2024-04-23 08:49:36 -0700

    Moved JSONSafeEncoding to v2.0 (#335)
    * Moved JSONSafeEncoding to v2.0
    
    * Added time to iOS test until apple fixes it.
    
    * Added stub for autoreleasepool on linux.

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/segmentio/sovran-swift.git", from: "1.1.1"),
-        .package(url: "https://github.com/segmentio/jsonsafeencoding-swift.git", from: "1.0.2")
+        .package(url: "https://github.com/segmentio/jsonsafeencoding-swift.git", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
