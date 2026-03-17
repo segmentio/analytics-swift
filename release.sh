2abf07f7df47867584816b9eb2d5c8adbbe60bec
Brandon Sneed <brandon@redf.net>
2026-03-17 14:22:24 -0700

    update release script

--- release.sh
+++ release.sh
@@ -46,15 +46,15 @@ fi
 
 # check if `swift-create-xcframework` tool is installed.
 # command will return non-zero if not.
-if ! command -v swift-create-xcframework &> /dev/null
-then
-	echo "Swift's create-xcframework tool is required, but could not be found."
-	echo "Install it via:"
-    echo "    $ brew install mint"
-    echo "    $ mint install unsignedapps/swift-create-xcframework"
-    echo ""
-	exit 1
-fi
+# if ! command -v swift-create-xcframework &> /dev/null
+# then
+# 	echo "Swift's create-xcframework tool is required, but could not be found."
+# 	echo "Install it via:"
+#     echo "    $ brew install mint"
+#     echo "    $ mint install unsignedapps/swift-create-xcframework"
+#     echo ""
+# 	exit 1
+# fi
 
 # check if `gh` tool has auth access.
 # command will return non-zero if not auth'd.
